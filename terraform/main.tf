2b9c0688a8e5cb113401190dced45eb1e02f9f30
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2020-12-30 16:09:52 +0900

    Add Terraform files

---
+++ terraform/main.tf
@@ -0,0 +1,28 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "airflow-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket        = "yuyat-apache-airflow-test"
+  acl           = "private"
+  force_destroy = false
+
+  versioning {
+    enabled = true
+  }
+}

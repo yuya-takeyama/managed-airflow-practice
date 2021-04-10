02146dfe98e147c7faeb19aefb2ab7f507b0d675
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2021-04-11 06:23:52 +0900

    Setup CodeBuild projects

---
+++ terraform/codebuild-airflow.tf
@@ -0,0 +1,117 @@
+resource "aws_codebuild_project" "airflow" {
+  artifacts {
+    encryption_disabled    = "false"
+    override_artifact_name = "false"
+    type                   = "NO_ARTIFACTS"
+  }
+
+  badge_enabled = "false"
+  build_timeout = "60"
+
+  cache {
+    type = "NO_CACHE"
+  }
+
+  description    = "https://github.com/yuya-takeyama/managed-airflow-practice"
+  encryption_key = "arn:aws:kms:ap-northeast-1:711930837542:alias/aws/s3"
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:4.0"
+    image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = "false"
+    type                        = "LINUX_CONTAINER"
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name  = "codebuild"
+      status      = "ENABLED"
+      stream_name = "managed-airflow-practice-airflow"
+    }
+
+    s3_logs {
+      encryption_disabled = "false"
+      status              = "DISABLED"
+    }
+  }
+
+  name           = "managed-airflow-practice-airflow"
+  queued_timeout = "480"
+  service_role   = "arn:aws:iam::711930837542:role/service-role/codebuild-terraform-service-role"
+
+  source {
+    buildspec       = ".codebuild/buildspec.yml"
+    git_clone_depth = "1"
+
+    git_submodules_config {
+      fetch_submodules = "false"
+    }
+
+    insecure_ssl        = "false"
+    location            = "https://github.com/yuya-takeyama/managed-airflow-practice.git"
+    report_build_status = "true"
+    type                = "GITHUB"
+  }
+}
+
+data "aws_iam_policy_document" "codebuild-airflow-assume-role" {
+  version = "2012-10-17"
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["codebuild.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "codebuild-airflow" {
+  name               = "codebuild-managed-airflow-practice-airflow"
+  assume_role_policy = data.aws_iam_policy_document.codebuild-airflow-assume-role.json
+}
+
+data "aws_iam_policy_document" "codebuild-airflow-s3" {
+  version = "2012-10-17"
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket",
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject",
+    ]
+    resources = [
+      "arn:aws:s3:::yuyat-apache-airflow-test",
+      "arn:aws:s3:::yuyat-apache-airflow-test/*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "codebuild-airflow" {
+  name   = "codebuild-managed-airflow-practice-airflow"
+  policy = data.aws_iam_policy_document.codebuild-airflow-s3.json
+}
+
+resource "aws_iam_policy_attachment" "codebuild-airflow" {
+  name       = "codebuild"
+  roles      = [aws_iam_role.codebuild-airflow.name]
+  policy_arn = aws_iam_policy.codebuild-airflow.arn
+}
+
+resource "aws_codebuild_webhook" "airflow" {
+  project_name = aws_codebuild_project.airflow.name
+
+  filter_group {
+    filter {
+      type    = "EVENT"
+      pattern = "PUSH"
+    }
+
+    filter {
+      type    = "FILE_PATH"
+      pattern = "^environments/.*"
+    }
+  }
+}
---
+++ terraform/codebuild-terraform.tf
@@ -0,0 +1,95 @@
+resource "aws_codebuild_project" "terraform" {
+  artifacts {
+    encryption_disabled    = "false"
+    override_artifact_name = "false"
+    type                   = "NO_ARTIFACTS"
+  }
+
+  badge_enabled = "false"
+  build_timeout = "60"
+
+  cache {
+    type = "NO_CACHE"
+  }
+
+  description    = "https://github.com/yuya-takeyama/managed-airflow-practice"
+  encryption_key = "arn:aws:kms:ap-northeast-1:711930837542:alias/aws/s3"
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:4.0"
+    image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = "false"
+    type                        = "LINUX_CONTAINER"
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name  = "codebuild"
+      status      = "ENABLED"
+      stream_name = "managed-airflow-practice-terraform"
+    }
+
+    s3_logs {
+      encryption_disabled = "false"
+      status              = "DISABLED"
+    }
+  }
+
+  name           = "managed-airflow-practice-terraform"
+  queued_timeout = "480"
+  service_role   = "arn:aws:iam::711930837542:role/service-role/codebuild-terraform-service-role"
+
+  source {
+    buildspec       = ".codebuild/buildspec.yml"
+    git_clone_depth = "1"
+
+    git_submodules_config {
+      fetch_submodules = "false"
+    }
+
+    insecure_ssl        = "false"
+    location            = "https://github.com/yuya-takeyama/managed-airflow-practice.git"
+    report_build_status = "true"
+    type                = "GITHUB"
+  }
+}
+
+data "aws_iam_policy_document" "codebuild-terraform-assume-role" {
+  version = "2012-10-17"
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["codebuild.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "codebuild-terraform" {
+  name               = "codebuild-managed-airflow-practice-terraform"
+  assume_role_policy = data.aws_iam_policy_document.codebuild-terraform-assume-role.json
+}
+
+resource "aws_iam_policy_attachment" "codebuild-terraform" {
+  name       = "codebuild"
+  roles      = [aws_iam_role.codebuild-terraform.name]
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_codebuild_webhook" "terraform" {
+  project_name = aws_codebuild_project.terraform.name
+
+  filter_group {
+    filter {
+      type    = "EVENT"
+      pattern = "PUSH"
+    }
+
+    filter {
+      type    = "FILE_PATH"
+      pattern = "^terraformm/.*"
+    }
+  }
+}

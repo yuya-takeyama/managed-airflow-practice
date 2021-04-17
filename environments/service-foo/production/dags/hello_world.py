79216678500e5e8c0dcbbdb80a0a2681ff22c13e
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2021-04-18 02:23:36 +0900

    Test and lint DAGs

--- environments/service-foo/production/dags/hello_world.py
+++ environments/service-foo/production/dags/hello_world.py
@@ -4,9 +4,11 @@ import logging
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
+
 def greet_hello():
     logging.info("Hello World")
 
-dag = DAG("FirstDag", start_date=datetime.datetime.now(),schedule_interval=None)
 
-first_task = PythonOperator(python_callable=greet_hello , dag=dag , task_id="first-task")
+dag = DAG("FirstDag", start_date=datetime.datetime.now(), schedule_interval=None)
+
+first_task = PythonOperator(python_callable=greet_hello, dag=dag, task_id="first-task")
